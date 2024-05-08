16c3ffeec37610a119f0d80994dea9a8fe557994
ElizPN <pankevych.liza@gmail.com>
2024-05-08 20:09:14 +0200

    templates and some functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,28 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'test';
+  public name: string;
+  public surName: string;
+  squareRootOfTwo: number;
+  public arr: number[];
+  public user: {name: string, surn: string};
+  public text: string;
+
+  public showName() {
+    return 'Olov';
+
+  }
+
+  constructor() {
+    this.squareRootOfTwo = Math.sqrt(2);
+    this.name = "john"
+    this.surName = "Smit"
+    this.arr = [ 1, 2, 3, 4, 5]
+    this.user =  {
+      name: 'liza',
+      surn: 'romanova',
+    }
+    this.text = 'abcde';
+
+  }
 }
