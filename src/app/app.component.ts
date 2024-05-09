e14dbe6c54f278d553a978d21e78e13f8372a276
ElizPN <pankevych.liza@gmail.com>
2024-05-09 09:20:17 +0200

    ngIf directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'test';
@@ -14,24 +15,28 @@ export class AppComponent {
   public surName: string;
   squareRootOfTwo: number;
   public arr: number[];
-  public user: {name: string, surn: string};
+  public user: { name: string; surn: string };
   public text: string;
 
   public showName() {
     return 'Olov';
-
   }
+  public isAdmin: boolean;
+  public num: number;
+  public age: number;
 
   constructor() {
     this.squareRootOfTwo = Math.sqrt(2);
-    this.name = "john"
-    this.surName = "Smit"
-    this.arr = [ 1, 2, 3, 4, 5]
-    this.user =  {
+    this.name = 'john';
+    this.surName = 'Smit';
+    this.arr = [1, 2, 3, 4, 5];
+    this.user = {
       name: 'liza',
       surn: 'romanova',
-    }
+    };
     this.text = 'abcde';
-
+    this.isAdmin = true;
+    this.num = 1;
+    this.age = 18;
   }
 }
